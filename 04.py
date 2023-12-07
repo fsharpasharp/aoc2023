c28549a4a227ee9045a9cbad8e15b95400da7d9c
fsharpasharp <jnliuu@gmail.com>
2023-12-07 12:14:58 +0100

    Add 04

--- 04.py
+++ 04.py
@@ -3,4 +3,26 @@ import re
 with open('input', 'r') as f:
     lines = f.read().splitlines()
 
-print(lines)
+number = '\d+'
+score = 0
+extras = dict()
+for i, line in enumerate(lines):
+    left, right = line.split('|')
+    winners = map(int, re.findall(number, left)[1:]) # skip the first number
+    numbers = map(int, re.findall(number, right))
+    intersection = set(winners) & set(numbers)
+
+    number_of_scratch_cards = extras.get(i, 1)
+    number_of_winners = len(intersection)
+    for j in range (i+1, i+number_of_winners+1):
+        extras[j] = extras.get(j, 1) + number_of_scratch_cards
+
+    if intersection:
+        score += 2 ** (number_of_winners-1)
+
+scratch_cards = 0
+for i in range(len(lines)):
+    scratch_cards += extras.get(i, 1)
+
+print(score)
+print(scratch_cards)
